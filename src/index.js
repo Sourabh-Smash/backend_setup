f6f1edf7dc433a1b855eb4d008efa2c0cdcfa6c2
Sourabh-Smash <souramena123@gmail.com>
2024-05-28 15:55:24 +0530

    wrote schema and models

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
-connectDB();
+const backendPort = process.env.PORT || 4000;
+connectDB()
+  .then(() => {
+    // error listening for an event
+    app.on("error", (error) => {
+      console.error("Error occured at app.on : ", error);
+      throw error;
+    });
+    app.listen(backendPort, () =>
+      console.log(`Server is running at port : ${backendPort}`)
+    );
+  })
+  .catch((error) => {
+    console.error("Mango DB connection failed !");
+    throw error;
+  });
