fb232afedc7b7f0946284d9a873940ac08379102
Sourabh-Smash <souramena123@gmail.com>
2024-05-29 13:52:37 +0530

    add setup configurations done

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../uitls/asyncHandler.js";
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "ok",
+  });
+});
+
+export { registerUser };
